af818aa7a9b27117151da15a04b9554b5e8f0789
Maruf9270 <Maruf9270@gmail.com>
2022-11-08 08:58:52 +0600

    Added database to the project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
 // Declearing port
@@ -9,9 +9,26 @@ const port = process.env.PORT || 4000;
 // Using middleware
 app.use(cors());
 app.use(express.json());
+
+// Mongodb configs
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.acms3da.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Checking if it gets connected to database
+client.connect(err => {
+ if(err){
+    console.log(err);
+    console.log('Thre is a internal error');
+ }
+ else{
+    console.log('connected to database');
+ }
+ 
+  client.close();
+});
+
 // Route for testing purpose
 app.get('/',(req,res)=>{res.send('Server is running')})
-
 
 // Fireng up the server
 app.listen(port, ()=>{console.log(`Youe server is running on port:${port}`);})
